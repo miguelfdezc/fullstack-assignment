00c7ec05cf988d3b744f4dae435fb095e6a41b4c
Miguel Fernandez Carratala <miguelfdezc99@gmail.com>
2023-01-22 14:36:10 +0100

    Add TimeSerieSchema to CurrencySchema

--- services/api/src/currency/infrastructure/schema/mongoose-currency.schema.ts
+++ services/api/src/currency/infrastructure/schema/mongoose-currency.schema.ts
@@ -1,9 +1,22 @@
 import { model, Schema } from "mongoose";
 
+const TimeSerieSchema = new Schema(
+  {
+    date: Date,
+    open: Number,
+    high: Number,
+    low: Number,
+    close: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
 const CurrencySchema = new Schema(
   {
     code: String,
     hasSubscription: Boolean,
+    history: [TimeSerieSchema]
   },
   {
     timestamps: true,
