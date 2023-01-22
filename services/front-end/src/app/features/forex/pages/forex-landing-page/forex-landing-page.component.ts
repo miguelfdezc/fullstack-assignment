cbd2433df875e3f5803ec5b240e3e263c69c6924
Miguel Fernandez Carratala <miguelfdezc99@gmail.com>
2023-01-22 20:06:43 +0100

    WIP

--- services/front-end/src/app/features/forex/pages/forex-landing-page/forex-landing-page.component.ts
+++ services/front-end/src/app/features/forex/pages/forex-landing-page/forex-landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Currency } from '../../currency';
+import { CurrencyService } from '../../currency.service';
 
 @Component({
   selector: 'app-forex-landing-page',
@@ -6,42 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forex-landing-page.component.scss']
 })
 export class ForexLandingPageComponent implements OnInit {
-  currencies: any[] = [];
+  currencies: Currency[] = [];
 
-  constructor() {
-    this.currencies = [
-      {
-        "_id": "63cc6470b9d6bf7da69b8dd1",
-        "_code": "USD",
-        "_hasSubscription": true,
-        "_history": [
-          {
-            "date": "2023-01-20T00:00:00.000Z",
-            "open": 1.08341,
-            "high": 1.0859,
-            "low": 1.0801,
-            "close": 1.08556,
-            "_id": "63cd36da07267199680765d5",
-            "updatedAt": "2023-01-22T13:15:06.713Z",
-            "createdAt": "2023-01-22T13:15:06.713Z"
-          },
-          {
-            "date": "2023-01-19T00:00:00.000Z",
-            "open": 1.07928,
-            "high": 1.084,
-            "low": 1.0781,
-            "close": 1.08308,
-            "_id": "63cd36da07267199680765d6",
-            "updatedAt": "2023-01-22T13:15:06.713Z",
-            "createdAt": "2023-01-22T13:15:06.713Z"
-          }
-        ]
-      }
-    ];
-  }
+  constructor(private currencyService: CurrencyService) {}
 
   ngOnInit(): void {
-
+    this.getCurrencies();
   }
 
+  getCurrencies(): void {
+    this.currencyService.findAllSubscribedCurrencies()
+    .subscribe(response => this.currencies = response.data);
+  }
+
+  subscribe(code: string): void {
+    this.currencyService.subscribe(code);
+  }
+
+  unsubscribe(code: string): void {
+    this.currencyService.unsubscribe(code);
+  }
 }
