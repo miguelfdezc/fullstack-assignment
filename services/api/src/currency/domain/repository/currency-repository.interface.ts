2434490b247f6c1c339e9b7804e2d69ea9528911
Miguel Fernandez Carratala <miguelfdezc99@gmail.com>
2023-01-22 14:46:37 +0100

    Add retrieveForexData to ICurrencyRepository

--- services/api/src/currency/domain/repository/currency-repository.interface.ts
+++ services/api/src/currency/domain/repository/currency-repository.interface.ts
@@ -1,8 +1,10 @@
 import { Nullable } from "src/utils";
+import { TimeSerie } from "../models";
 import { Currency } from "../models/currency";
 
 export interface ICurrencyRepository {
   update(currency: Currency): Promise<void>;
   findAllSubscriptions(): Promise<Currency[]>;
   findByCode(code: string): Promise<Nullable<Currency>>;
+  retrieveForexData(code: string): Promise<TimeSerie[]>;
 }
